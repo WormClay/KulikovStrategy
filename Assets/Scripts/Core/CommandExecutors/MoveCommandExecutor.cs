b8e370529c743e5973a2f9488586a66f3de5b12a
alexandr.kulikov <a.kulikov@tascredit.kz>
2023-03-31 21:05:12 +0600

    Lesson4
    Lesson4

--- Assets/Scripts/Core/CommandExecutors/MoveCommandExecutor.cs
+++ Assets/Scripts/Core/CommandExecutors/MoveCommandExecutor.cs
@@ -1,8 +1,32 @@
+using System.Threading;
 using UnityEngine;
+using UnityEngine.AI;
 public class MoveCommandExecutor : CommandExecutorBase<IMoveCommand>
 {
-    public override void ExecuteSpecificCommand(IMoveCommand command)
+    [SerializeField] private UnitMovementStop _stop;
+    [SerializeField] private Animator _animator;
+    [SerializeField] private StopCommandExecutor _stopCommandExecutor;
+    public override async void ExecuteSpecificCommand(IMoveCommand command)
     {
-        Debug.Log($"{name} is moving to {command.Target}!");
+        GetComponent<NavMeshAgent>().destination = command.Target;
+        _animator.SetTrigger(Animator.StringToHash("Walk"));
+        _stopCommandExecutor.CancellationTokenSource = new CancellationTokenSource();
+        try
+        {
+            await _stop
+            .WithCancellation
+            (
+            _stopCommandExecutor
+            .CancellationTokenSource
+            .Token
+            );
+        }
+        catch
+        {
+            GetComponent<NavMeshAgent>().isStopped = true;
+            GetComponent<NavMeshAgent>().ResetPath();
+        }
+        _stopCommandExecutor.CancellationTokenSource = null;
+        _animator.SetTrigger(Animator.StringToHash("Idle"));
     }
 }
